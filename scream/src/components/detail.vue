48a9fa8db3d590cc4bb9a40c41920b086e0c85db
Mr_Xie <xqm_1021@163.com>
2018-11-21 09:58:22 +0800

    scream

---
+++ scream/src/components/detail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div >
+    card
+  </div>
+</template>
+  
+<script>
+export default {
+  name: 'detail',
+  data () {
+    return {
+    }
+  },
+
+  methods:{
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+ 
+</style>
